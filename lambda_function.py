2ebdea494e16e494fafc3dc1a671cf10679ee8f6
Yash Mav <y.mav@somaiya.edu>
2024-03-29 13:51:19 +0530

    aws cicd demo2

--- lambda_function.py
+++ lambda_function.py
@@ -8,7 +8,7 @@ def lambda_handler(event,context):
     response = requests.get("https://www.google.com/")
     print(response.text)
 
-    d =  {'col1':[1,2] ,'col2':[3,4]}
+    d =  {'col1':[1,2,0] ,'col2':[3,4,5]}
     df = pd.DataFrame(d)
     print(df)
     print("Done.....")
